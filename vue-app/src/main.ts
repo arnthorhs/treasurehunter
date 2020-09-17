7cff70442bce71d11a0dfd31f9a6f9797e101ed2
Arnthor <arnthorhs@gmail.com>
2020-09-17 14:59:05 +0200

    move routes to separate file

--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -1,34 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
-import Home from './components/Home.vue';
-import Shop from './components/Shop.vue';
-import Character from './components/Character.vue';
-import NotFound from './components/NotFound.vue';
-
+import routes from './routes'
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/shop',
-      component:Shop
-    },
-    {
-      path: '/character',
-      component: Character,
-    },
-    {
-      path: '*',
-      component: NotFound
-    }
-
-  ]
+  routes
 });
 
 Vue.config.productionTip = false;
--- vue-app/src/routes.ts
+++ vue-app/src/routes.ts
@@ -1,4 +1,23 @@
-export default {
-    '/': 'Home',
-    '/about': 'About'
+import Home from './components/Home.vue';
+import Shop from './components/Shop.vue';
+import Character from './components/Character.vue';
+import NotFound from './components/NotFound.vue';
+
+export default [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/shop',
+    component:Shop
+  },
+  {
+    path: '/character',
+    component: Character,
+  },
+  {
+    path: '*',
+    component: NotFound
   }
+]
